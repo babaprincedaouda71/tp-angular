7de7bce3c7b61418ca3af65ea3cb011660313651
Baba Prince <babadaouda01@gmail.com>
2023-06-13 17:12:14 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'enset-app';
+  actions : Array<any> = [
+    {
+      title : "Home",
+      "route": "/home",
+      icon : "house"
+    },
+    {
+      title : "Products",
+      "route": "/products",
+      icon : "basket2-fill"
+    },
+    {
+      title : "New Product",
+      "route": "/newProduct",
+      icon : "plus-circle"
+    }
+  ];
+  currentAction : any
+
+  setCurrentAction(action: any) {
+    this.currentAction = action
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { NewProductComponent } from './new-product/new-product.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    NewProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule, //Pour faire des requêtes http vers le server
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
